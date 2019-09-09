f36524b660d70a8aaf51ad3c47ca31162316299a
EL MOSSAOUI Redouan <redouan.el-mossaoui@cegedim.com>
2019-09-09 16:21:45 +0100

    NDLOG-0: init a nodeJs logger.

---
+++ test.mjs
@@ -0,0 +1,11 @@
+import logger from './lib/logger.mjs'
+
+logger.info('first message 1')
+
+logger.info('second message 2')
+
+logger.info('third message 3')
+
+logger.error('first error 1')
+
+logger.warn('first warning 1')
